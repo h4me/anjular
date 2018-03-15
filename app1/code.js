47c8bd9230a1a3639dbd8bc14c807547cf37ab22
pawepiot <pawel.piotrowicz@intel.com>
2018-03-15 16:48:48 +0100

    broadcast it works

--- app1/code.js
+++ app1/code.js
@@ -1,5 +1,6 @@
-    angular.module('todoApp', [])
-      .controller('TodoListController', function() {
+var app = angular.module('todoApp', [])
+/*
+app.controller('TodoListController', function() {
         var todoList = this;
         todoList.todos = [
           {text:'learn AngularJS', done:true},
@@ -26,3 +27,49 @@
           });
         };
       });
+
+app.controller('Example', function() {
+          var main = this;
+          main.jTable = [ {name:'learn AngularJS', phone: '4444'},
+                         {name:'build an AngularJS app', phone : '22222'}];
+
+});
+*/
+
+app.controller('mainCtrl', function($scope) {
+     console.log('main')
+
+    $scope.Send2Me = function() {
+       $scope.$broadcast('myCustomEvent', '!!!! Broadcast it works! !!!');
+    }
+
+});
+
+
+app.controller('Example2', function($scope,$rootScope) {
+console.log('init Example2')
+
+      $scope.jTable = [
+                    {name:'Example2', phone: '4444'},
+                    {name:'build an AngularJS app', phone : '22222'}
+                  ];
+
+       $scope.$on('myCustomEvent', function (event, data) {
+                  //  $scope.jeden=data; // 'Some data'
+                    console.log('Cos przyslo')
+                    console.log(data)
+       });
+      // $rootScope.sharedata = 'sharedata is ok!';
+
+});
+
+
+
+app.controller('Example1', function($scope) {
+  console.log('init Example1')
+  $scope.jTable = [ {name:'learn AngularJS', phone: '4444'},
+                 {name:'build an AngularJS app', phone : '22222'}];
+  $scope.reader='default'
+//  $scope.reader
+  $scope.sharedata = 'share data'
+});
